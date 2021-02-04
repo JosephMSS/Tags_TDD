bf6b64d48a88c701e7ffe57f1792bc5ce0a48f08
JosephMMS <josephzx1@hotmail.com>
2021-02-04 00:29:41 -0600

    Testing Http

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+    public function test_empty()
+    { 
+        // $this->withExceptionHandling();
+        $this->get('/')
+        ->assertStatus(200)
+        ->assertSee('No hay etiquetas');
+    }
+}
